08a291fdbf66df1c3568f4315a306eb1c14e8f98
Ana <anafvana@gmail.com>
2021-01-01 16:51:46 +0100

    Create database & tables; parse credentials.

--- server.go
+++ server.go
@@ -1,8 +1,12 @@
 package main
 
 import (
-	//"net/http"
 	"database/sql"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
 	
 	"github.com/labstack/echo/v4"
 	_ "github.com/go-sql-driver/mysql"
@@ -13,6 +17,12 @@ type server struct {
 	db 	*sql.DB
 }
 
+type cred struct {
+	username string `json:"username"`
+	password string `json:"password"`
+	database string `json:"database"`
+}
+
 func (s *server) getCategory(c echo.Context) error {
 	return c.JSON(http.StatusOK, ???)
 }
@@ -21,8 +31,23 @@ func (s *server) getKeyword(c echo.Context) error{
 	return c.JSON(http.StatusOK, ???)
 }
 
+func creds(fn string) string{
+	f, err := os.Open(fn)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer f.Close()
+
+	bytes, _ := ioutil.ReadAll(f)
+
+	var c cred
+
+	json.Unmarshal(bytes, &c)
+	return fmt.Sprintf("%v:%v/%v", c.username, c.password, c.database)
+}
+
 func main() {
-	db, err := sql.Open()
+	db, err := sql.Open("mysql", creds("credentials.json"))
 	s := &server{
 		e:	echo.New(),
 		db:	db,
