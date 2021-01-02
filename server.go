3e3a34266e8dc3d159a3f4b6735595d563b5ba5a
Ana <anafvana@gmail.com>
2021-01-02 02:42:55 +0100

    Formatted + tiny change to insert entries into DB (failing)

--- server.go
+++ server.go
@@ -7,16 +7,16 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
-	"time"
 	"strconv"
-	
-	"github.com/labstack/echo/v4"
+	"time"
+
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/labstack/echo/v4"
 )
 
 type server struct {
-	e 	*echo.Echo
-	db 	*sql.DB
+	e  *echo.Echo
+	db *sql.DB
 }
 
 type cred struct {
@@ -38,13 +38,13 @@ type keyword struct {
 }
 
 type entry struct {
-	ID 				int				`json:"_id"`
-	Question 		string			`json:"question"`	
-	Answer 			string			`json:"answer"`
-	SubmissionDate 	time.Time		`json:"submissionDate"`
-	LastUpdate 		time.Time		`json:"lastUpdate"`
-	Categories 		[]category		`json:"categories"`
-	KeyWords 		[]keyword		`json:"keywords"`
+	ID             string     `json:"_id"`
+	Question       string     `json:"question"`
+	Answer         string     `json:"answer"`
+	SubmissionDate time.Time  `json:"submissionDate"`
+	LastUpdate     time.Time  `json:"lastUpdate"`
+	Categories     []category `json:"categories"`
+	KeyWords       []keyword  `json:"keywords"`
 }
 
 /*func (s *server) getCategory(c echo.Context) error {
@@ -55,9 +55,11 @@ func (s *server) getKeyword(c echo.Context) error{
 	return c.JSON(http.StatusOK, ???)
 }*/
 
-func creds(fn string) string{
+func creds(fn string) string {
 	f, err := os.Open(fn)
-	if err != nil {	fmt.Println(err) }
+	if err != nil {
+		fmt.Println(err)
+	}
 	defer f.Close()
 
 	bytes, _ := ioutil.ReadAll(f)
@@ -68,9 +70,11 @@ func creds(fn string) string{
 	return fmt.Sprintf("%v:%v@/%v", c.Username, c.Password, c.Database)
 }
 
-func readEntriesJSON(fn string) []entry{
+func readEntriesJSON(fn string) []entry {
 	f, err := os.Open(fn)
-	if err != nil {	fmt.Println(err) }
+	if err != nil {
+		fmt.Println(err)
+	}
 	defer f.Close()
 
 	bytes, _ := ioutil.ReadAll(f)
@@ -78,41 +82,48 @@ func readEntriesJSON(fn string) []entry{
 	var allEntries []entry
 
 	err = json.Unmarshal(bytes, &allEntries)
-	if err != nil {	fmt.Println(err) }
+	if err != nil {
+		fmt.Println(err)
+	}
 
 	//TODO: Erase test
-	for i := range allEntries {
+	/* for i := range allEntries {
 		fmt.Println(allEntries[i])
-	}
+	} */
 
 	return allEntries
 }
 
-func (c *category) UnmarshalJSON(data []byte) error{
+func (c *category) UnmarshalJSON(data []byte) error {
 	var v string
 	err := json.Unmarshal(data, &v)
-	if err != nil { fmt.Println(err) }
+	if err != nil {
+		fmt.Println(err)
+	}
 	c.Name = v
-	return err 
+	return err
 }
 
-func (kw *keyword) UnmarshalJSON(data []byte) error{
+func (kw *keyword) UnmarshalJSON(data []byte) error {
 	var v string
 	err := json.Unmarshal(data, &v)
-	if err != nil { fmt.Println(err) }
+	if err != nil {
+		fmt.Println(err)
+	}
 	kw.Name = v
-	return err 
+	return err
 }
 
 func (s *server) createTables() error {
 	//TODO delete this before official deployment
-	tableNames := []string {"entriesKeywords", "entriesCategories", "categories", "keywords", "entries"}
-	for i := range tableNames{
+	tableNames := []string{"entriesKeywords", "entriesCategories", "categories", "keywords", "entries"}
+	for i := range tableNames {
 		q := fmt.Sprintf("DROP TABLE IF EXISTS %v ;", tableNames[i])
 		_, err := s.db.Exec(q)
-		if err != nil { fmt.Println(err) }
+		if err != nil {
+			fmt.Println(err)
+		}
 	}
-	
 
 	q1 := `
 	CREATE TABLE entries(
@@ -155,17 +166,27 @@ func (s *server) createTables() error {
 		FOREIGN KEY ( entryID ) REFERENCES entries( idEntry ),
 		FOREIGN KEY ( keyword ) REFERENCES keywords( keyword )
 	);`
-		
+
 	_, err := s.db.Exec(q1)
-	if err != nil { fmt.Println(err) }
+	if err != nil {
+		fmt.Println(err)
+	}
 	_, err = s.db.Exec(q2)
-	if err != nil { fmt.Println(err) }
+	if err != nil {
+		fmt.Println(err)
+	}
 	_, err = s.db.Exec(q3)
-	if err != nil { fmt.Println(err) }
+	if err != nil {
+		fmt.Println(err)
+	}
 	_, err = s.db.Exec(q4)
-	if err != nil { fmt.Println(err) }
+	if err != nil {
+		fmt.Println(err)
+	}
 	_, err = s.db.Exec(q5)
-	if err != nil { fmt.Println(err) }
+	if err != nil {
+		fmt.Println(err)
+	}
 
 	return err
 }
@@ -173,64 +194,76 @@ func (s *server) createTables() error {
 /* func (s *server) getEntries() error{
 } */
 
-func (s *server) getCategoryNames(c echo.Context) error{
+func (s *server) getCategoryNames(c echo.Context) error {
 	rows, _ := s.db.Query("SELECT * FROM categories;")
 
 	var categoryName string
 	var cats []category
-	
+
 	for rows.Next() {
 		err := rows.Scan(&categoryName)
-		if err != nil { fmt.Println(err) }
+		if err != nil {
+			fmt.Println(err)
+		}
 		cats = append(cats, category{Name: categoryName})
 	}
-	
+
 	return c.JSON(http.StatusOK, cats)
 }
 
-func (s *server) getKeywordList(c echo.Context) error{
+func (s *server) getKeywordList(c echo.Context) error {
 	rows, _ := s.db.Query("SELECT * FROM keywords;")
 
 	var kw string
 	var kws []keyword
-	
+
 	for rows.Next() {
 		err := rows.Scan(&kw)
-		if err != nil { fmt.Println(err) }
+		if err != nil {
+			fmt.Println(err)
+		}
 		kws = append(kws, keyword{Name: kw})
 	}
-	
+
 	return c.JSON(http.StatusOK, kws)
 }
 
-func (s *server) newEntryDB(e entry) error{
+func (s *server) newEntryDB(e entry) error {
 	_, err := s.db.Exec(`
 	INSERT INTO entries (question, answer, submission_date, last_update)
 	VALUES (
 		?, ?, ?, ?
 	);`, e.Question, e.Answer, e.SubmissionDate, e.LastUpdate)
-	if err != nil { fmt.Println(err) }
-	
+	if err != nil {
+		fmt.Println(err)
+	}
+
 	var entryID string
 
 	err = s.db.QueryRow(`
 		SELECT LAST_INSERT_ID
 		FROM entries;
 	`).Scan(entryID)
-	if err != nil { fmt.Println(err) }
+	if err != nil {
+		fmt.Println(err)
+	}
 
 	for i := range e.Categories {
 		b, err := s.checkCategory(e.Categories[i])
-		if err != nil { fmt.Println(err) }
-		if !b{
+		if err != nil {
+			fmt.Println(err)
+		}
+		if !b {
 			_, err := s.db.Exec(`
 			INSERT INTO categories
 			VALUES (
 				?
 			);`, e.Categories[i])
-			if err != nil { fmt.Println(err) }
+			if err != nil {
+				fmt.Println(err)
+			}
 		}
-		
+
 		_, err = s.db.Exec(`
 			INSERT INTO entriesCategories (entryID, category)
 			SET entryId = (
@@ -243,21 +276,27 @@ func (s *server) newEntryDB(e entry) error{
 					FROM categories
 					WHERE category = ?
 				);`, entryID, e.Categories[i])
-		if err != nil { fmt.Println(err) }
+		if err != nil {
+			fmt.Println(err)
+		}
 	}
 
 	for i := range e.KeyWords {
 		b, err := s.checkKeyword(e.KeyWords[i])
-		if err != nil { fmt.Println(err) }
-		if !b{
+		if err != nil {
+			fmt.Println(err)
+		}
+		if !b {
 			_, err := s.db.Exec(`
 			INSERT INTO keywords
 			VALUES (
 				?
 			);`, e.KeyWords[i])
-			if err != nil { fmt.Println(err) }
+			if err != nil {
+				fmt.Println(err)
+			}
 		}
-		
+
 		_, err = s.db.Exec(`
 			INSERT INTO entriesKeywords (entryID, keyword)
 			SET entryId = (
@@ -270,17 +309,21 @@ func (s *server) newEntryDB(e entry) error{
 					FROM categories
 					WHERE category = ?
 				);`, entryID, e.KeyWords[i])
-		if err != nil { fmt.Println(err) }
+		if err != nil {
+			fmt.Println(err)
+		}
 	}
 	return err
 }
 
-func (s *server) checkCategory(c category) (bool, error){
+func (s *server) checkCategory(c category) (bool, error) {
 	var cat string
 	err := s.db.QueryRow(`
 		SELECT COUNT(category) FROM categories WHERE category=? ;
 	`, c.Name).Scan(&cat)
-	if err != nil { fmt.Println(err) }
+	if err != nil {
+		fmt.Println(err)
+	}
 
 	catNr, err := strconv.ParseInt(cat, 6, 12)
 
@@ -294,12 +337,14 @@ func (s *server) checkCategory(c category) (bool, error){
 	return b, err
 }
 
-func (s *server) checkKeyword(kw keyword) (bool, error){
+func (s *server) checkKeyword(kw keyword) (bool, error) {
 	var k string
 	err := s.db.QueryRow(`
 		SELECT COUNT(keyword) FROM keywords WHERE keyword=? ;
 	`, kw.Name).Scan(&k)
-	if err != nil { fmt.Println(err) }
+	if err != nil {
+		fmt.Println(err)
+	}
 
 	kNr, err := strconv.ParseInt(k, 6, 12)
 
@@ -337,16 +382,20 @@ func (s *server) checkKeyword(kw keyword) (bool, error){
 	e.KeyWords = kkww
 } */
 
-
 func main() {
-db, err := sql.Open("mysql", creds("credentials.json"))
+	db, err := sql.Open("mysql", creds("credentials.json"))
 	s := &server{
-		e:	echo.New(),
-		db:	db,
+		e:  echo.New(),
+		db: db,
 	}
-	if err != nil { fmt.Println(err) }
-	s.createTables();
-	readEntriesJSON("dummy.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+	s.createTables()
+	entries := readEntriesJSON("dummy.json")
+	for i := range entries {
+		s.newEntryDB(entries[i])
+	}
 	//s.e.POST("/tables", s.createTables)
 	//s.e.GET("/cat", s.getCategory)
 	//s.e.GET("/kw", s.getKeyword)
